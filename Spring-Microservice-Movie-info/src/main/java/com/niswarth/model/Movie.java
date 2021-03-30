bc589016c917911542421f5d6b92fc3176e4227e
Niswarth <niswarthshetty@gmail.com>
2021-03-30 17:00:05 +0530

    Final Commit

---
+++ Spring-Microservice-Movie-info/src/main/java/com/niswarth/model/Movie.java
@@ -0,0 +1,29 @@
+package com.niswarth.model;
+
+public class Movie {
+	private String movieId;
+	private String movieName;
+	
+	public Movie(String movieId, String movieName) {
+		super();
+		this.movieId = movieId;
+		this.movieName = movieName;
+	}
+
+	public String getMovieId() {
+		return movieId;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+}
---
+++ Spring-Microservice-Movie-info/src/test/java/com/niswarth/SpringMicroserviceMovieInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niswarth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceMovieInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
